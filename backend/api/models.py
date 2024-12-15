bff3f9882aeb316c70676389cee7bd2427554883
Christian Salvadora <christianrsalvadora@gmail.com>
2024-12-15 13:40:01 +0800

    🗃️Update Task default values

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    is_completed = models.BooleanField()
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
     date_created = models.DateField(auto_now_add=True)
-    date_completed = models.DateField()
+    date_completed = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
