230a3b56a021bd59a5b22e7adf0e59938fcb7811
Christian Salvadora <christianrsalvadora@gmail.com>
2024-12-15 15:49:07 +0800

    🛂Update token lifetime

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,4 +130,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),  # Set access token expiration (e.g., 15 minutes)
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     # Set refresh token expiration (e.g., 1 day)
 }
