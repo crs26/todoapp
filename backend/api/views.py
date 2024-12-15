26e1cf89d4a95f74ec89e5378ba2f664e51fe8e1
Christian Salvadora <christianrsalvadora@gmail.com>
2024-12-15 13:40:41 +0800

    ✨Added api to fetch and create task

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializer import TaskSerializer
+from .models import Task
 
-# Create your views here.
+class TaskAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        task = Task.objects.all()
+        return Response(task)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
