5127449ae275a2037a0e8f89174cc243ae9272e6
Christian Salvadora <christianrsalvadora@gmail.com>
2024-12-15 15:39:59 +0800

    🛂Add authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializer import TaskSerializer
 from .models import Task
 
 class TaskAPIView(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure user is authenticated
+
     def get(self, request, *args, **kwargs):
         task = Task.objects.all()
-        return Response(task)
+        serializer = TaskSerializer(task, many=True)
+        return Response(serializer.data)
     
     def post(self, request, *args, **kwargs):
         serializer = TaskSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
